258db94adb941987f6f767094f927b6cb38cde0b
Arun Singh <arunsingh28aug.as@gmail.com>
2021-10-28 15:33:56 +0530

    database connection export

--- src/server.js
+++ src/server.js
@@ -3,13 +3,15 @@ const crypto = require("crypto");
 require("dotenv").config();
 const connectionToDB = require("./utils/DB");
 
+// init express
 const app = express();
 
-// porting
+// dyanmic porting
 const port = process.env.PORT || 4000;
 
-connectionToDB();
-
+// routing 
 app.use("/", require("./Router/index"));
 
+
+// server start
 app.listen(port, console.log(`Server is up on PORT: ${port}`));
